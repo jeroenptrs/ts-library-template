ddbf06c792df6843ff0a09e2f38efca7248d74ec
Jeroen <contact@jeroenpeeters.me>
2023-07-29 01:44:36 +0200

    feat: add rollup, effect, type-fest and ts-jest

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export const add = (a: number, b: number): number => a + b;
+import { Effect } from "effect";
+import { Integer } from "type-fest/source/numeric";
+
+export const add = <T extends number>(a: Integer<T>, b: Integer<T>): number =>
+  Effect.runSync(Effect.succeed(a + b));
